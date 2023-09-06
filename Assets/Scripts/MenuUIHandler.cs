437943d28e3e6b890c7b1792ff11c0fa1a0cea92
nhanvan4779 <nhanvan4779@gmail.com>
2023-09-06 18:20:22 +0700

    add menu scene
    - add start - level 1 button
    - add exit button

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartGame(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}

d819506229cf440d93089a9c644fd7a0f8b07eae
nhanvan4779 <nhanvan4779@gmail.com>
2023-09-09 08:22:40 +0700

    add new menu
    - add credits menu

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -8,6 +8,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject startMenuGroup;
+
+    [SerializeField] private GameObject creditsGroup;
+
+    private void Awake()
+    {
+        creditsGroup.SetActive(false);
+    }
+
     public void StartGame(int level)
     {
         SceneManager.LoadScene(level);
@@ -20,5 +29,17 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    public void ShowCredits()
+    {
+        startMenuGroup.SetActive(false);
+        creditsGroup.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        creditsGroup.SetActive(false);
+        startMenuGroup.SetActive(true);
     }
 }
